e5a2ee03994463e7a255ee4eeccc83d746eb2bc9
dinara <zhunisbekova.dinara@gmail.com>
2020-03-15 20:33:25 +0700

    secondcommit

--- file.sh
+++ file.sh
@@ -3,14 +3,14 @@ echo -e "Автор: Жунисбекова Динара\nНазвание: Ко
 
 while [[ $choice != 3 ]]
 do 
-	echo -e "\n Выберите нужное действие:\n\t 1-Izmenite format izovrajeni\n\t 2 - izmenite razreshenie picture\n\t 3 - Exit"
+	echo -e "\n Выберите нужное действие:\n\t 1 - Изменить формат изображения\n\t 2 - Изменить разрешение изображения\n\t 3 - Выход"
 	read choice
 
 	if [[ $choice = 1 ]]
 	then 
-		echo -e "Vvedite put' faila:\n"
+		echo -e "Введите путь файла:\n"
 		read name
-		echo -e "Choose the right format of pic:\n\t 1 - .jpg\n\t 2 - .png\n\t 3 - .bmp\n\t 4 - .ico"
+		echo -e "Выберите необходимый формат файла:\n\t 1 - .jpg\n\t 2 - .png\n\t 3 - .bmp\n\t 4 - .ico"
 		read doing 
 		case $doing in
 			1)
@@ -26,15 +26,15 @@ do
 			mogrify -format ico $name
 			;;
 		*)
-	echo "Fuck off"
+	echo "Вы ввели неправильное действие"
 	esac
 
 else
 	if [[ $choice = 2 ]]
 	then 
-		echo -e "vvedite put faila:\n"
+		echo -e "Введите путь файла:\n"
 	       read name
-       echo -e "Vvedite razresshenie of pic:"
+       echo -e "Введите необходимое разрешение изображения:"
 read size 
 mogrify -resize $size $name
 fi
